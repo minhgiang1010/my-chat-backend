1e31baaaa3b05ba903b729a43225a19dcc0ea7d1
minhgiang1010 <leminhgiang113@gmail.com>
2025-10-17 17:25:20 +0700

    up

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ export const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
